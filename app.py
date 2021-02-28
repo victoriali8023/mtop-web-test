0e9f408eff19fb2f660c233bb414145365fbd921
Yuxuan Li <54911077+victoriali8023@users.noreply.github.com>
2021-02-28 14:49:20 -0500

    update

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def index():
     # thirdSession = ['31', '32']
 
     # letter = random.choice(letterList)
-    letter = 'F'
+    letter = 'K'
     random.shuffle(scenarioList)
     number = str(uuid.uuid4())
     # first = letter + random.choice(firstSession)
